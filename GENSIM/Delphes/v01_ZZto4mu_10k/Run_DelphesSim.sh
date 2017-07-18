ee536b415fd176d261adc65d5df411cdd5ad16e8
Kyoengpil Lee <kplee@cern.ch>
2017-07-19 02:05:38 +0900

    delphes over zz

--- GENSIM/Delphes/v01_ZZto4mu_10k/Run_DelphesSim.sh
+++ GENSIM/Delphes/v01_ZZto4mu_10k/Run_DelphesSim.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-export KP_INPUT_PATH=/home/kplee/Physics/ZZTo4L/Madgraph/GEN_v01_ZZ4mu_10k/ZZTo4L01j_5f_NLO_FXFX/Events/run_02/input.hep
+KP_INPUT_PATH=/home/kplee/Physics/ZZTo4L/Madgraph/GEN_v01_ZZ4mu_10k/ZZTo4L01j_5f_NLO_FXFX/Events/run_03/events_PYTHIA8_0.hepmc
 
 # -- DelphesHepMC card outputname inputname -- #
 $KP_DELPHES_PATH/DelphesHepMC $KP_DELPHES_PATH/cards/delphes_card_CMS.tcl $KP_DELPHESDATA_PATH/ZZTo4mu_10k.root $KP_INPUT_PATH # -- error
