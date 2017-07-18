b218d40d211346ba0a408bb116f04d7d9d2ddb71
Kyoengpil Lee <kplee@cern.ch>
2017-07-19 01:04:47 +0900

    use ROOT5

--- ValidationPlots/run_Hist_Validation.sh
+++ ValidationPlots/run_Hist_Validation.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+export ROOTSYS="/programs/root"
+source $ROOTSYS/bin/thisroot.sh
+
 root -l -b << EOF
 
 TString DelphesPath = gSystem->Getenv("KP_DELPHES_PATH")
@@ -9,5 +12,8 @@ gSystem->Load(DelphesPath+"/libDelphes.so")
 .q
 
 EOF
+
+export ROOTSYS="/cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-ikhhed6/"
+source $ROOTSYS/bin/thisroot.sh
 
 echo "finished"
