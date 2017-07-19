9da51c91b034e8addbbf1d94d797bfea7e16c4a4
Kyoengpil Lee <kplee@cern.ch>
2017-07-19 16:01:02 +0900

    M(Zp)=2000 case

--- GENSIM/Madgraph/GEN_v06_ZprimeTo4L_MZp_2000/run_GenerateEvents.sh
+++ GENSIM/Madgraph/GEN_v06_ZprimeTo4L_MZp_2000/run_GenerateEvents.sh
@@ -3,10 +3,10 @@
 date
 
 # -- setup the cards -- #
-# cp run_card.dat ./zprime_b3ltau_pp-4l/Cards/
+cp run_card.dat ./zprime_b3ltau_pp-4l/Cards/
 # cp shower_card.dat ./zprime_b3ltau_pp-4l/Cards/
 
-zprime_b3ltau_pp-4l/bin/madevent run_GenerateEvents_MZp_2000.dat
+zprime_b3ltau_pp-4l/bin/madevent run_GenerateEvents.dat
 
 echo "Generation is finished"
 
