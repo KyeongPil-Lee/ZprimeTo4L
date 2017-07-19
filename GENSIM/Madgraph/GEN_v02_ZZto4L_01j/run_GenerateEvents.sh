4e90c3fa9e72d293c00ee787634e45c184bb9a83
Kyoengpil Lee <kplee@cern.ch>
2017-07-19 09:53:24 +0900

    ZZ->4l generation

---
+++ GENSIM/Madgraph/GEN_v02_ZZto4L_01j/run_GenerateEvents.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+date
+
+# -- setup the cards -- #
+cp run_card.dat ./ZZTo4L01j_5f_NLO_FXFX/Cards/
+cp shower_card.dat ./ZZTo4L01j_5f_NLO_FXFX/Cards/
+
+ZZTo4L01j_5f_NLO_FXFX/bin/aMCatNLO run_GenerateEvents.dat
+
+echo "Generation is finished"
+
+date
---
+++ GENSIM/Madgraph/GEN_v02_ZZto4L_01j/run_MECalc.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+date
+
+export KP_MADGRAPH_PATH=/home/kplee/Physics/Madgraph/MG5_aMC_v2_5_5
+
+$KP_MADGRAPH_PATH/bin/mg5_aMC proc_card.dat
+
+echo "matrix element calculation is finished: ZZTo4L01j_5f_NLO_FXFX directory is created with outputs"
+echo "Next step: ZZTo4L01j_5f_NLO_FXFX/bin/aMCatNLO"
+
+date
