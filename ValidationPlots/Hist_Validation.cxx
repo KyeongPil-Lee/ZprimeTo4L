50fd7a196ca06c0ed9715a70dcbae0513d03007a
Kyoengpil Lee <kplee@cern.ch>
2017-07-26 21:30:11 +0900

    plots for 1M events

--- ValidationPlots/Hist_Validation.cxx
+++ ValidationPlots/Hist_Validation.cxx
@@ -3,10 +3,10 @@
 void Hist_Validation()
 {
 	TString DataPath = gSystem->Getenv("KP_DATA_PATH");
-	TString InputFileName = DataPath+"/Delphes/v20170707_1st_DetSim_Delphes/evts_1gen_m1000_m10_13TeV_200k.root";
-	Bool_t _IsSignal = kFALSE;
+	TString InputFileName = "";
+	Bool_t Flag_IsSignal = kFALSE;
 
-	InputFileName = DataPath+"/Delphes/v20170720_1st_DetSim_Delphes_200k/ZZto4L0j_200k.root"; Flag_IsSignal = kFALSE;
+	InputFileName = DataPath+"/Delphes/v20170720_1st_DetSim_Delphes_200k/ZZto4L0j_1M.root"; Flag_IsSignal = kFALSE;
 
 	Analyzer *analyzer = new Analyzer(InputFileName);
 	analyzer->IsSignal( _IsSignal );
