1fa4a43b774094ba6a4557684637f0fbd9571805
Kyoengpil Lee <kplee@cern.ch>
2017-07-27 15:24:09 +0900

    update with MyLepton class

--- ValidationPlots/HistTools.h
+++ ValidationPlots/HistTools.h
@@ -1,3 +1,542 @@
+Bool_t CompareGenParticle( GenParticle* Object1, GenParticle* Object2 )
+{
+	return Object1->PT > Object2->PT;
+}
+
+Bool_t CompareMuon( Muon* Muon1, Muon* Muon2 )
+{
+	return Muon1->PT > Muon2->PT;
+}
+
+Bool_t CompareElectron( Electron* Electron1, Electron* Electron2 )
+{
+	return Electron1->PT > Electron2->PT;
+}
+
+class MyGenPair
+{
+public:
+	GenParticle *First;
+	GenParticle *Second;
+
+	Double_t M;
+	Double_t Pt;
+	Double_t Rap;
+	TLorentzVector LVec_P;
+
+	MyGenPair( GenParticle* GenPar1, GenParticle* GenPar2 )
+	{
+		if( GenPar1->PT > GenPar2->PT )
+		{
+			this->First = GenPar1;
+			this->Second = GenPar2;
+		}
+		else
+		{
+			this->First = GenPar2;
+			this->Second = GenPar1;
+		}
+
+		this->Calc_Var();
+	}
+
+	void Calc_Var()
+	{
+		TLorentzVector LVec_First = this->First->P4();
+		TLorentzVector LVec_Second = this->Second->P4();
+		this->LVec_P = LVec_First + LVec_Second;
+
+		this->M = this->LVec_P.M();
+		this->Pt = this->LVec_P.Pt();
+		this->Rap = this->LVec_P.Rapidity();
+	}
+};
+
+
+class MyLepton
+{
+public:
+	Double_t Pt;
+	Double_t Eta;
+	Double_t Phi;
+	Double_t Px;
+	Double_t Py;
+	Double_t Pz;
+	TLorentzVector LVec_P;
+
+	Bool_t isMuon;
+	Muon *Mu;
+
+	Bool_t isElectron;
+	Electron *Elec;
+	GenParticle* GenLepton;
+
+	MyLepton()
+	{
+		this->Pt = 0;
+		this->Eta = 0;
+		this->Phi = 0;
+		this->Px = 0;
+		this->Py = 0;
+		this->Pz = 0;
+		this->LVec_P.SetPxPyPzE(0,0,0,0);
+
+		this->isMu = kFALSE;
+		this->Mu = NULL;
+
+		this->isElec = kFALSE;
+		this->Elec = NULL;
+
+		this->GenLepton = NULL;
+	}
+
+	MyLepton( Muon *_Muon ) : MyLepton()
+	{
+		this->Pt = _Muon->PT;
+		this->Eta = _Muon->Eta;
+		this->Phi = _Muon->Phi;
+		this->Px = _Muon->Px;
+		this->Py = _Muon->Py;
+		this->Pz = _Muon->Pz;
+		this->LVec_P = _Muon->P4();
+
+		this->isMu = kTRUE;
+		this->Mu = _Muon;
+		this->GenLepton = (GenParticle*)_Muon->Particle.GetObject();
+	}
+
+	MyLepton( Electron *_Electron ) : MyLepton()
+	{
+		this->Pt = _Electron->PT;
+		this->Eta = _Electron->Eta;
+		this->Phi = _Electron->Phi;
+		this->Px = _Electron->Px;
+		this->Py = _Electron->Py;
+		this->Pz = _Electron->Pz;
+		this->LVec_P = _Electron->P4();
+
+		this->isElec = kTRUE;
+		this->Elec = _Electron;
+		this->GenLepton = (GenParticle*)_Electron->Particle.GetObject();
+	}
+};
+
+Bool_t CompareLepton( MyLepton* Lepton1, MyLepton* Lepton2 )
+{
+	return Lepton1->PT > Lepton2->PT;
+}
+
+class MyLeptonPair
+{
+public:
+	MyLepton *First;
+	MyLepton *Second;
+
+	Double_t M;
+	Double_t Pt;
+	Double_t Rap;
+	TLorentzVector LVec_P;
+
+	MyGenPair* GENPair_postFSR;
+
+
+	MyLeptonPair( MyLepton* Lepton1, MyLepton* Lepton2 )
+	{
+		if( Lepton1->PT > Lepton2->PT )
+		{
+			this->First = Lepton1;
+			this->Second = Lepton2;
+		}
+		else
+		{
+			this->First = Lepton2;
+			this->Second = Lepton1;
+		}
+
+		this->Calc_Var();
+	}
+
+	void Calc_Var()
+	{
+		TLorentzVector LVec_First = First->P4();
+		TLorentzVector LVec_Second = Second->P4();
+		this->LVec_P = LVec_First + LVec_Second;
+
+		this->M = this->LVec_P.M();
+		this->Pt = this->LVec_P.Pt();
+		this->Rap = this->LVec_P.Rapidity();
+
+		// -- matched Gen-pair @ post-FSR level (final state) -- //
+		GenParticle* GenLepton1 = this->First->GenLepton;
+		GenParticle* GenLepton2 = this->Second->GenLepton;
+		this->GENPair_postFSR = new MyGenPair( GenLepton1, GenLepton2 );
+	}
+};
+
+class MyMuonPair
+{
+public:
+	Muon *First;
+	Muon *Second;
+
+	Double_t M;
+	Double_t Pt;
+	Double_t Rap;
+	TLorentzVector LVec_P;
+
+	MyGenPair* GENPair_postFSR;
+
+
+	MyMuonPair( Muon* mu1, Muon* mu2 )
+	{
+		if( mu1->PT > mu2->PT )
+		{
+			this->First = mu1;
+			this->Second = mu2;
+		}
+		else
+		{
+			this->First = mu2;
+			this->Second = mu1;
+		}
+
+		this->Calc_Var();
+	}
+
+	void Calc_Var()
+	{
+		TLorentzVector LVec_First = First->P4();
+		TLorentzVector LVec_Second = Second->P4();
+		this->LVec_P = LVec_First + LVec_Second;
+
+		this->M = this->LVec_P.M();
+		this->Pt = this->LVec_P.Pt();
+		this->Rap = this->LVec_P.Rapidity();
+
+		// -- matched Gen-pair @ post-FSR level (final state) -- //
+		GenParticle* GenMu1 = (GenParticle*)this->First->Particle.GetObject();
+		GenParticle* GenMu2 = (GenParticle*)this->Second->Particle.GetObject();
+		this->GENPair_postFSR = new MyGenPair( GenMu1, GenMu2 );
+	}
+};
+
+class HistContainer_4GENLepton
+{
+public:
+	TString Type;
+
+	TH1D* h_Pt;
+	TH1D* h_Rap;
+	TH1D* h_Mass;
+
+	HistContainer_4GENLepton(TString _Type)
+	{
+		this->Type = _Type;
+		this->Init_Hist();
+	}
+
+	void Fill( MyGenPair* GenPair1, MyGenPair* GenPair2, Double_t weight = 1.0 )
+	{
+		TLorentzVector LVec_P_4Lep = GenPair1->LVec_P + GenPair2->LVec_P;
+
+		Double_t Pt = LVec_P_4Lep.Pt();
+		Double_t Rap = LVec_P_4Lep.Rapidity();
+		Double_t Mass = LVec_P_4Lep.M();
+
+		this->h_Pt->Fill( Pt, weight );
+		this->h_Rap->Fill( Rap, weight );
+		this->h_Mass->Fill( Mass, weight );
+	}
+
+	void Save( TFile *f_output )
+	{
+		f_output->cd();
+
+		this->h_Pt->Write();
+		this->h_Rap->Write();
+		this->h_Mass->Write();
+	}
+
+	void Init_Hist()
+	{
+		this->h_Pt = new TH1D("h_Pt_4GenLep_"+this->Type, "", 10000, 0, 10000);
+		this->h_Rap = new TH1D("h_Rap_4GenLep_"+this->Type, "", 200, -10, 10);
+		this->h_Mass = new TH1D("h_Mass_4GenLep_"+this->Type, "", 80, -4, 4);
+	}
+};
+
+class HistContainer_4Lepton
+{
+public:
+	TString Type;
+
+	TH1D* h_Pt;
+	TH1D* h_Rap;
+	TH1D* h_Mass;
+	TH1D* h_RelDiff_Mass;
+
+	HistContainer_4Lepton(TString _Type)
+	{
+		this->Type = _Type;
+		this->Init_Hist();
+	}
+
+	void Fill( MyLeptonPair* LeptonPair1, MyLeptonPair* LeptonPair2, Double_t weight = 1.0 )
+	{
+		TLorentzVector LVec_P_4Lep = LeptonPair1->LVec_P + LeptonPair2->LVec_P;
+
+		Double_t Pt = LVec_P_4Lep.Pt();
+		Double_t Rap = LVec_P_4Lep.Rapidity();
+		Double_t Mass = LVec_P_4Lep.M();
+
+		Double_t Mass_GEN = (LeptonPair1->GENPair_postFSR->LVec_P + LeptonPair2->GENPair_postFSR->LVec_P).M();
+		Double_t RelDiff_Mass = (Mass - Mass_GEN) / Mass_GEN;
+
+		this->h_Pt->Fill( Pt, weight );
+		this->h_Rap->Fill( Rap, weight );
+		this->h_Mass->Fill( Mass, weight );
+		this->h_RelDiff_Mass->Fill( RelDiff_Mass, weight );
+	}
+
+	void Save( TFile *f_output )
+	{
+		f_output->cd();
+
+		this->h_Pt->Write();
+		this->h_Rap->Write();
+		this->h_Mass->Write();
+		this->h_RelDiff_Mass->Write();
+	}
+
+	void Init_Hist()
+	{
+		this->h_Pt = new TH1D("h_Pt_4Lepton_"+this->Type, "", 10000, 0, 10000);
+		this->h_Rap = new TH1D("h_Rap_4Lepton_"+this->Type, "", 200, -10, 10);
+		this->h_Mass = new TH1D("h_Mass_4Lepton_"+this->Type, "", 80, -4, 4);
+		this->h_RelDiff_Mass = new TH1D("h_RelDiff_Mass_4Lepton_"+this->Type, "", 1000, -1, 1);
+	}
+};
+
+class HistContainer_GENLeptonPair
+{
+public:
+	TString Type;
+
+	TH1D* h_Pt;
+	TH1D* h_Rap;
+	TH1D* h_Mass;
+
+	HistContainer_GENLeptonPair(TString _Type)
+	{
+		this->Type = _Type;
+		this->Init_Hist();
+	}
+
+	void Fill( MyGenPair* GenPair, Double_t weight = 1.0 )
+	{
+		Double_t Pt = GenPair->Pt;
+		Double_t Rap = GenPair->Rap;
+		Double_t Mass = GenPair->M;
+
+		this->h_Pt->Fill( Pt, weight );
+		this->h_Rap->Fill( Rap, weight );
+		this->h_Mass->Fill( Mass, weight );
+	}
+
+	void Save( TFile *f_output )
+	{
+		f_output->cd();
+
+		this->h_Pt->Write();
+		this->h_Rap->Write();
+		this->h_Mass->Write();
+	}
+
+	void Init_Hist()
+	{
+		this->h_Pt = new TH1D("h_Pt_2Lepton_GEN_"+this->Type, "", 10000, 0, 10000);
+		this->h_Rap = new TH1D("h_Rap_2Lepton_GEN_"+this->Type, "", 200, -10, 10);
+		this->h_Mass = new TH1D("h_Mass_2Lepton_GEN_"+this->Type, "", 80, -4, 4);
+	}
+};
+
+class HistContainer_LeptonPair
+{
+public:
+	TString Type;
+
+	TH1D* h_Pt;
+	TH1D* h_Rap;
+	TH1D* h_Mass;
+	TH1D* h_RelDiff_Mass;
+
+	HistContainer_LeptonPair(TString _Type)
+	{
+		this->Type = _Type;
+		this->Init_Hist();
+	}
+
+	void Fill( MyLeptonPair* LeptonPair, Double_t weight = 1.0 )
+	{
+		Double_t Pt = LeptonPair->Pt;
+		Double_t Rap = LeptonPair->Rap;
+		Double_t Mass = LeptonPair->M;
+
+		Double_t Mass_GEN = LeptonPair->GENPair_postFSR->M;
+		Double_t RelDiff_Mass = (Mass - Mass_GEN ) / Mass_GEN; // -- difference with "post-FSR" gen-mass -- //
+
+		this->h_Pt->Fill( Pt, weight );
+		this->h_Rap->Fill( Rap, weight );
+		this->h_Mass->Fill( Mass, weight );
+		this->h_RelDiff_Mass->Fill( RelDiff_Mass, weight );
+	}
+
+	void Save( TFile *f_output )
+	{
+		f_output->cd();
+
+		this->h_Pt->Write();
+		this->h_Rap->Write();
+		this->h_Mass->Write();
+		this->h_RelDiff_Mass->Write();
+	}
+
+	void Init_Hist()
+	{
+		this->h_Pt = new TH1D("h_Pt_2Lepton_"+this->Type, "", 10000, 0, 10000);
+		this->h_Rap = new TH1D("h_Rap_2Lepton_"+this->Type, "", 200, -10, 10);
+		this->h_Mass = new TH1D("h_Mass_2Lepton_"+this->Type, "", 80, -4, 4);
+		this->h_RelDiff_Mass = new TH1D("h_RelDiff_Mass_2Lepton_"+this->Type, "", 1000, -1, 1);
+	}
+};
+
+class HistContainer_GENLepton
+{
+public:
+	TString Type;
+
+	TH1D* h_Pt;
+	TH1D* h_Eta;
+	TH1D* h_Phi;
+
+	HistContainer_GENLepton(TString _Type)
+	{
+		this->Type = _Type;
+		this->Init_Hist();
+	}
+
+	void Fill( GenParticle* GenPar, Double_t weight = 1.0 )
+	{
+		Double_t Pt = GenPar->PT;
+		Double_t Eta = GenPar->Eta;
+		Double_t Phi = GenPar->Phi;
+
+		this->h_Pt->Fill( Pt, weight );
+		this->h_Eta->Fill( Eta, weight );
+		this->h_Phi->Fill( Phi, weight );
+	}
+
+	void Save( TFile *f_output )
+	{
+		f_output->cd();
+
+		this->h_Pt->Write();
+		this->h_Eta->Write();
+		this->h_Phi->Write();
+	}
+
+	void Init_Hist()
+	{
+		this->h_Pt = new TH1D("h_Pt_GEN_"+this->Type, "", 10000, 0, 10000);
+		this->h_Eta = new TH1D("h_Eta_GEN_"+this->Type, "", 200, -10, 10);
+		this->h_Phi = new TH1D("h_Phi_GEN_"+this->Type, "", 80, -4, 4);
+	}
+
+};
+
+
+class HistContainer_Lepton
+{
+public:
+	TString Type;
+
+	TH1D* h_Pt;
+	TH1D* h_Eta;
+	TH1D* h_Phi;
+	TH1D* h_Pt_GEN;
+	TH1D* h_Eta_GEN;
+	TH1D* h_Phi_GEN;
+	TH1D* h_RelDiff_Pt;
+	TH1D* h_RelDiff_Eta;
+	TH1D* h_RelDiff_Phi;
+
+	HistContainer_Lepton( TString _Type )
+	{
+		this->Type = _Type;
+
+		this->Init_Hist();
+	}
+
+	void Fill( MyLepton *Lepton, Double_t weight = 1.0 )
+	{
+		Double_t Pt_RECO = Lepton->PT;
+		Double_t Eta_RECO = Lepton->Eta;
+		Double_t Phi_RECO = Lepton->Phi;
+
+		GenParticle* GenLepton = (GenParticle*)Lepton->Particle.GetObject();
+		Double_t Pt_GEN = GenLepton->PT;
+		Double_t Eta_GEN = GenLepton->Eta;
+		Double_t Phi_GEN = GenLepton->Phi;
+
+		Double_t RelDiff_Pt = (Pt_RECO - Pt_GEN) / Pt_GEN;
+		Double_t RelDiff_Eta = (Eta_RECO - Eta_GEN) / Eta_GEN;
+		Double_t RelDiff_Phi = (Phi_RECO - Phi_GEN) / Phi_GEN;
+
+		this->h_Pt->Fill( Pt_RECO, weight );
+		this->h_Eta->Fill( Eta_RECO, weight );
+		this->h_Phi->Fill( Phi_RECO, weight );
+
+		this->h_Pt_GEN->Fill( Pt_GEN, weight );
+		this->h_Eta_GEN->Fill( Eta_GEN, weight );
+		this->h_Phi_GEN->Fill( Phi_GEN, weight );
+
+		this->h_RelDiff_Pt->Fill( RelDiff_Pt, weight );
+		this->h_RelDiff_Eta->Fill( RelDiff_Eta, weight );
+		this->h_RelDiff_Phi->Fill( RelDiff_Phi, weight );
+	}
+
+	void Init_Hist()
+	{
+		this->h_Pt = new TH1D("h_Pt_"+this->Type, "", 10000, 0, 10000);
+		this->h_Eta = new TH1D("h_Eta_"+this->Type, "", 60, -3, 3);
+		this->h_Phi = new TH1D("h_Phi_"+this->Type, "", 80, -4, 4);
+
+		this->h_Pt_GEN = new TH1D("h_Pt_MatchedGEN_"+this->Type, "", 10000, 0, 10000);
+		this->h_Eta_GEN = new TH1D("h_Eta_MatchedGEN_"+this->Type, "", 60, -3, 3);
+		this->h_Phi_GEN = new TH1D("h_Phi_MatchedGEN_"+this->Type, "", 80, -4, 4);
+
+		this->h_RelDiff_Pt = new TH1D("h_RelDiff_Pt_"+this->Type, "", 1000, -1, 1);
+		this->h_RelDiff_Eta = new TH1D("h_RelDiff_Eta_"+this->Type, "", 1000, -1, 1);
+		this->h_RelDiff_Phi = new TH1D("h_RelDiff_Phi_"+this->Type, "", 1000, -1, 1);
+	}
+
+	void Save( TFile *f_output )
+	{
+		f_output->cd();
+
+		this->h_Pt->Write();
+		this->h_Eta->Write();
+		this->h_Phi->Write();
+		this->h_Pt_GEN->Write();
+		this->h_Eta_GEN->Write();
+		this->h_Phi_GEN->Write();
+		this->h_RelDiff_Pt->Write();
+		this->h_RelDiff_Eta->Write();
+		this->h_RelDiff_Phi->Write();
+	}
+};
+
+
 void Print_GenParticle( GenParticle* GenPar )
 {
 	printf("[ID=%03d, Status=%02d]: (M1, M2) = (%03d, %03d), (D1, D2) = (%03d, %03d), (Pt, eta, phi) = (%9.3lf, %9.3lf, %9.3lf)\n",
@@ -270,6 +809,48 @@ Bool_t Matching_RECO_GENMuonHPFS( vector<GenParticle*> vec_Lepton_FinalState, TC
 
 	// -- check whether "ALL" of muons are matched to the final state gen-leptons from hard process -- //
 	if( vec_MuonMatched.size() == vec_Lepton_FinalState.size() )
+		Flag_Success = kTRUE;
+
+	return Flag_Success;
+}
+
+Bool_t Matching_RECO_GENMuonHPFS( vector<GenParticle*> vec_Lepton_FinalState, vector< MyLepton* > vec_RECOLepton, vector< MyLepton* >& vec_LeptonMatched )
+{
+	Bool_t Flag_Success = kFALSE;
+
+	Int_t nRECOLepton = (Int_t)vec_RECOLepton.size();
+
+	for( const auto& GenMu_FS : vec_Lepton_FinalState )
+	{
+		// printf("[GenMu_FS] (Pt, Eta, Phi) = (%9.3lf, %9.3lf, %9.3lf)\n", GenMu_FS->PT, GenMu_FS->Eta, GenMu_FS->Phi);
+		for(Int_t i_lep=0; i_lep<nRECOLepton; i_lep++)
+		{
+			// cout << i_lep << "th reco muon" << endl;
+
+			MyLepton* RECOLepton = vec_RECOLepton[i_lep];
+			GenParticle* GENLepton = RECOLepton->GenLepton;
+
+			// Muon* RecoMu = (Muon*)Br_Muon->At(i_lep);
+			// GenParticle* GenMu = (GenParticle*)RecoMu->Particle.GetObject();
+			// printf("\t[GenMu matched to %02d muon] (Pt, Eta, Phi) = (%9.3lf, %9.3lf, %9.3lf)", i_mu, GenMu->PT, GenMu->Eta, GenMu->Phi);
+
+			// Bool_t isMatched = kFALSE;
+			// if( *(GenMu_FS) == *(GenMu) )
+			if( IsSame_GenParticle(GenMu_FS, GENLepton) )
+			{
+				// isMatched = kTRUE;
+				vec_LeptonMatched.push_back( RECOLepton );
+				break;
+			}
+
+			// cout << "-> isMatched = " << isMatched << endl;
+
+		} // -- end of reco-lepton iteration -- //
+
+	} // -- end of gen-lepton iteration -- //
+
+	// -- check whether "ALL" of muons are matched to the final state gen-leptons from hard process -- //
+	if( vec_LeptonMatched.size() == vec_Lepton_FinalState.size() )
 		Flag_Success = kTRUE;
 
 	return Flag_Success;
