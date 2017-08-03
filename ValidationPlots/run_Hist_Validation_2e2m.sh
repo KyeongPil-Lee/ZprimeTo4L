eb1fcd06cdf9a3f226cca6f8a6e05c7cd5a05642
Kyoengpil Lee <kplee@cern.ch>
2017-08-03 15:11:19 +0900

    for 2e2mu plots

---
+++ ValidationPlots/run_Hist_Validation_2e2m.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+root -l -b << EOF
+
+TString DelphesPath = gSystem->Getenv("KP_DELPHES_PATH")
+gSystem->Load(DelphesPath+"/libDelphes.so")
+.x Hist_Validation_2e2m.cxx++
+
+.q
+
+EOF
+
+echo "finished"
