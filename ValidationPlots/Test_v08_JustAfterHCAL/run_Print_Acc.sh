a25e5581bf71f0b53089faccd6300dbaa9af1599
Kyoengpil Lee <kplee@cern.ch>
2017-08-09 21:21:59 +0900

    test

---
+++ ValidationPlots/Test_v08_JustAfterHCAL/run_Print_Acc.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+root -l -b << EOF
+
+TString DelphesPath = gSystem->Getenv("KP_DELPHES_PATH")
+gSystem->Load(DelphesPath+"/libDelphes.so")
+.x Print_Acc.cxx++
+
+.q
+
+EOF
+
+echo "finished"
