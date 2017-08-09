a4968793b6097793cd270eaf5b938ecbc8211b92
Kyoengpil Lee <kplee@cern.ch>
2017-08-09 15:27:04 +0900

    test

---
+++ GENSIM/Delphes/Test_v07_BeforeECAL/Run_DelphesSim_Signal.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+$KP_DELPHES_PATH/DelphesHepMC delphes_card_CMS_BeforeECAL.tcl /home/kplee/data7/Delphes/v20170809_Test_2nd_BeforeECAL/MZp_4000_Msn3_1_200k.root /home/kplee/data7/LHE/v20170719_1st_ZprimeTo4L_AfterHadronization/MZp_4000_Msn3_1_200k.hepmc >&log1&
+$KP_DELPHES_PATH/DelphesHepMC delphes_card_CMS_BeforeECAL.tcl /home/kplee/data7/Delphes/v20170809_Test_2nd_BeforeECAL/MZp_4000_Msn3_40_200k.root /home/kplee/data7/LHE/v20170719_1st_ZprimeTo4L_AfterHadronization/MZp_4000_Msn3_40_200k.hepmc >&log2&
+$KP_DELPHES_PATH/DelphesHepMC delphes_card_CMS_BeforeECAL.tcl /home/kplee/data7/Delphes/v20170809_Test_2nd_BeforeECAL/MZp_4000_Msn3_50_200k.root /home/kplee/data7/LHE/v20170719_1st_ZprimeTo4L_AfterHadronization/MZp_4000_Msn3_50_200k.hepmc >&log3&
+$KP_DELPHES_PATH/DelphesHepMC delphes_card_CMS_BeforeECAL.tcl /home/kplee/data7/Delphes/v20170809_Test_2nd_BeforeECAL/MZp_4000_Msn3_1200_200k.root /home/kplee/data7/LHE/v20170719_1st_ZprimeTo4L_AfterHadronization/MZp_4000_Msn3_1200_200k.hepmc >&log4&
+
+echo "finished"
