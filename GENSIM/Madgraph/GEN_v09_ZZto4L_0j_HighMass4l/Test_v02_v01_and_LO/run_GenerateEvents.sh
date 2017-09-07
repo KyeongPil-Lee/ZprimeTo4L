1bc8803ba8e312c4af45454c798517d9db5230e5
Kyoengpil Lee <kplee@cern.ch>
2017-09-07 15:50:39 +0900

    modify them to be LO

--- GENSIM/Madgraph/GEN_v09_ZZto4L_0j_HighMass4l/Test_v02_v01_and_LO/run_GenerateEvents.sh
+++ GENSIM/Madgraph/GEN_v09_ZZto4L_0j_HighMass4l/Test_v02_v01_and_LO/run_GenerateEvents.sh
@@ -3,7 +3,7 @@
 date
 
 # -- setup the cards -- #
-#cp run_card.dat ./ZZTo4L0j_5f_NLO_FXFX/Cards/
+cp run_card.dat ./ZZTo4L0j_5f_NLO_FXFX/Cards/
 #cp shower_card.dat ./ZZTo4L0j_5f_NLO_FXFX/Cards/
 
 Local/MG5_Output/bin/madevent run_GenerateEvents.dat
