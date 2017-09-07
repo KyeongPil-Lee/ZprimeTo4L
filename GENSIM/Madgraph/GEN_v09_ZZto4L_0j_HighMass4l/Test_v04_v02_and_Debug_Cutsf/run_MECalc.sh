1f2db6a58291818ef8599f7f29e7595fb0f41b13
Kyoengpil Lee <kplee@cern.ch>
2017-09-07 18:00:43 +0900

    debug cuts.f

---
+++ GENSIM/Madgraph/GEN_v09_ZZto4L_0j_HighMass4l/Test_v04_v02_and_Debug_Cutsf/run_MECalc.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+date
+
+export KP_MADGRAPH_PATH=/home/kplee/Physics/Madgraph/MG5_aMC_v2_5_5
+
+# -- cuts.f with mass cut -- #
+cp cuts_DrCho.f $KP_MADGRAPH_PATH/Template/LO/SubProcesses/cuts.f
+
+$KP_MADGRAPH_PATH/bin/mg5_aMC proc_card.dat
+
+echo "matrix element calculation is finished: Local/MG5_Output directory is created with outputs"
+#echo "Next step: Local/MG5_Output/bin/aMCatNLO"
+
+# -- change with the orignal one -- #
+cp cuts_original_LO.f $KP_MADGRAPH_PATH/Template/LO/SubProcesses/cuts.f
+
+date
