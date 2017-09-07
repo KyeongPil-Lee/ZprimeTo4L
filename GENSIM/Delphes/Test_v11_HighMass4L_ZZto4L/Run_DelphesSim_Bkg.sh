2174c3fa3a37fe66604d2d71dc39b196cbcbc60e
Kyoengpil Lee <kplee@cern.ch>
2017-09-07 13:18:16 +0900

    delphes simulation for high-mass ZZto4L

---
+++ GENSIM/Delphes/Test_v11_HighMass4L_ZZto4L/Run_DelphesSim_Bkg.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+$KP_DELPHES_PATH/DelphesHepMC delphes_card_CMS_NoUniqueFinder.tcl /home/kplee/data7/Delphes/v20170907_1st_HighMass4L_ZZto4L/ZZto4L0j_M4L_100to2000_1M.root /home/kplee/data7/LHE/v20170907_1st_HighMass4L_ZZto4L/events_PYTHIA8_0.hepmc
+
+echo "simulation is finished"
