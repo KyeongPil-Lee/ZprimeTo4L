e7f4655928256e0792e55c06603ac11024e30e49
Kyoengpil Lee <kplee@cern.ch>
2017-09-12 01:32:39 +0900

    debug

--- GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v02_M1500to2500/run_GenerateEvents.sh
+++ GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v02_M1500to2500/run_GenerateEvents.sh
@@ -3,8 +3,8 @@
 date
 
 # -- setup the cards -- #
-cp run_card.dat ./Local/MG5_Output/
-#cp shower_card.dat ./Local/MG5_Output/
+cp run_card.dat ./Local/MG5_Output/Cards
+#cp shower_card.dat ./Local/MG5_Output/Cards
 
 Local/MG5_Output/bin/madevent run_GenerateEvents.dat
 
