8c835e1a89c6ec916966bab014efad9909dcf006
Kyoengpil Lee <kplee@cern.ch>
2017-09-11 23:50:20 +0900

    run script

---
+++ AccEff/v01_FirstTrial/Run.sh
@@ -0,0 +1,84 @@
+#!bin/bash
+
+cd Local/FourLepton_Reco_v20170822_125419/ZZto4L0j_1M
+source Run_FourLepton_Reco.sh >&../log_ZZto4L0j_1M&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_200_Msn3_1_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_200_Msn3_1_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_200_Msn3_50_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_200_Msn3_50_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_800_Msn3_1_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_800_Msn3_1_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_800_Msn3_50_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_800_Msn3_50_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_1000_Msn3_10_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_1000_Msn3_10_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_1000_Msn3_50_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_1000_Msn3_50_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_1000_Msn3_300_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_1000_Msn3_300_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_2000_Msn3_1_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_2000_Msn3_1_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_2000_Msn3_20_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_2000_Msn3_20_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_2000_Msn3_50_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_2000_Msn3_50_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_2000_Msn3_600_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_2000_Msn3_600_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_3000_Msn3_1_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_3000_Msn3_1_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_3000_Msn3_30_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_3000_Msn3_30_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_3000_Msn3_50_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_3000_Msn3_50_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_3000_Msn3_900_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_3000_Msn3_900_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_4000_Msn3_1_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_4000_Msn3_1_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_4000_Msn3_40_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_4000_Msn3_40_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_4000_Msn3_50_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_4000_Msn3_50_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+cd Local/FourLepton_Reco_v20170822_125419/MZp_4000_Msn3_1200_200k
+source Run_FourLepton_Reco.sh >&../log_MZp_4000_Msn3_1200_200k&
+cd /home/kplee/Physics/ZprimeTo4L/AccEff
+
+echo "Submission is finished"
+
