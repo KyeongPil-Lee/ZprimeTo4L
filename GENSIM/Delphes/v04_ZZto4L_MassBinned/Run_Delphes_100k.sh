9eef5696e9db17a0045a43387e5b57d633e36e95
Kyoengpil Lee <kplee@cern.ch>
2017-09-12 10:11:25 +0900

    debug

--- GENSIM/Delphes/v04_ZZto4L_MassBinned/Run_Delphes_100k.sh
+++ GENSIM/Delphes/v04_ZZto4L_MassBinned/Run_Delphes_100k.sh
@@ -1,8 +1,8 @@
 #!bin/bash
-cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v01_M500to1500/Local/MG5_Output/Events/run_03/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M500to1500.hepmc.gz
-cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v02_M1500to2500/Local/MG5_Output/Events/run_03/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M1500to2500.hepmc.gz
-cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v03_M2500to3500/Local/MG5_Output/Events/run_03/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M2500to3500.hepmc.gz
-cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v04_M3500toInf/Local/MG5_Output/Events/run_03/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M3500toInf.hepmc.gz
+cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v01_M500to1500/Local/MG5_Output/Events/run_02/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M500to1500.hepmc.gz
+cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v02_M1500to2500/Local/MG5_Output/Events/run_02/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M1500to2500.hepmc.gz
+cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v03_M2500to3500/Local/MG5_Output/Events/run_02/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M2500to3500.hepmc.gz
+cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v04_M3500toInf/Local/MG5_Output/Events/run_02/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M3500toInf.hepmc.gz
 
 cd /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width
 
