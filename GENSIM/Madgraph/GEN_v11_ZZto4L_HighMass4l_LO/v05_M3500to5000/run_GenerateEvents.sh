9560ac1ad2998af64a2e4e0ee2791ae07382fcff
Kyoengpil Lee <kplee@cern.ch>
2017-09-26 09:35:13 +0900

    add higher mass range

---
+++ GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v05_M3500to5000/run_GenerateEvents.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+date
+
+# -- setup the cards -- #
+cp run_card.dat ./Local/MG5_Output/Cards
+#cp shower_card.dat ./Local/MG5_Output/Cards
+
+Local/MG5_Output/bin/madevent run_GenerateEvents.dat
+
+echo "Generation is finished"
+
+date
