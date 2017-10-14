eea39a8cdf3186c11a002ac13a7af97f23688d22
Kyoengpil Lee <kplee@cern.ch>
2017-10-14 22:40:01 +0900

    delphes for high mass ZZ->4l samples

--- GENSIM/Delphes/v04_ZZto4L_MassBinned/Run_Delphes_HighMassSample.sh
+++ GENSIM/Delphes/v04_ZZto4L_MassBinned/Run_Delphes_HighMassSample.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v05_M3500to5000/Local/MG5_Output/Events/run_01/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M3500to5000.hepmc.gz
-cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v06_M3500to4500/Local/MG5_Output/Events/run_01/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M3500to4500.hepmc.gz
+cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v05_M3500to5000/Local/MG5_Output/Events/run_02/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M3500to5000.hepmc.gz
+cp /home/kplee/Physics/ZprimeTo4L/GENSIM/Madgraph/GEN_v11_ZZto4L_HighMass4l_LO/v06_M3500to4500/Local/MG5_Output/Events/run_02/tag_1_pythia8_events.hepmc.gz /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width/ZZto4L_M3500to4500.hepmc.gz
 
 cd /home/kplee/data7/Zprime4LSamples/v20170911_1st_DiffSeed_Width
 
