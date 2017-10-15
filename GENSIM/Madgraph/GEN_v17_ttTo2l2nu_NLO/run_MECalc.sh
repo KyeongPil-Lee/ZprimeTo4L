aefa153197ff935c08b013bac4dddc6340e17da9
Kyoengpil Lee <kplee@cern.ch>
2017-10-16 03:47:30 +0900

    setup for tt to 2l2nu

---
+++ GENSIM/Madgraph/GEN_v17_ttTo2l2nu_NLO/run_MECalc.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+date
+
+export KP_MADGRAPH_PATH=/home/kplee/Physics/Madgraph/MG5_aMC_v2_5_5
+
+$KP_MADGRAPH_PATH/bin/mg5_aMC proc_card.dat
+
+echo "matrix element calculation is finished: Local/MG5_Output directory is created with outputs"
+echo "Next step: Local/MG5_Output/bin/aMCatNLO"
+
+date
