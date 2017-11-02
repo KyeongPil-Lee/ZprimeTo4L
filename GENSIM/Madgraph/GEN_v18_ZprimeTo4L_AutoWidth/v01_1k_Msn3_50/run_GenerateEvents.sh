582337309957869fb8d66521c48037888fb94511
Kyoengpil Lee <kplee@cern.ch>
2017-11-02 12:17:26 +0900

    typo

--- GENSIM/Madgraph/GEN_v18_ZprimeTo4L_AutoWidth/v01_1k_Msn3_50/run_GenerateEvents.sh
+++ GENSIM/Madgraph/GEN_v18_ZprimeTo4L_AutoWidth/v01_1k_Msn3_50/run_GenerateEvents.sh
@@ -6,7 +6,7 @@ date
 cp run_card.dat ./Local/MG5_Output/Cards/
 # cp shower_card.dat ./Local/MG5_Output/Cards/
 
-./Local/bin/madevent run_GenerateEvents.dat
+./Local/MG5_Output/bin/madevent run_GenerateEvents.dat
 
 echo "Generation is finished"
 
